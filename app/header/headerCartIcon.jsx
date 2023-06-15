58777efdff49e9b4d6095b62898797619d6477ed
jules-sommer <rcsrc@pm.me>
2023-06-14 21:36:20 -0400

    some random changes trying to get docker to run

--- app/header/headerCartIcon.jsx
+++ app/header/headerCartIcon.jsx
@@ -7,32 +7,59 @@ import { useContext } from 'react';
 import NavLink from '../navLink';
 import { usePathname, useRouter } from 'next/navigation';
 import { CartStateContext, CartDispatchContext } from '../cartProvider';
+import { useIsClient } from '../isClientProvider';
 
 const HeaderCartIcon = () => {
 
 	const state = useContext(CartStateContext);
 	const dispatch = useContext(CartDispatchContext);
+	const isClient = useIsClient();
 
 	const router = useRouter();
 	const pathname = usePathname();
 
-	return (
+	if (isClient && pathname)
+		console.table({ pathname });
 
-		<nav className="flex select-none items-center justify-center">
-			<NavLink classes="!mr-0 items-center justify-center flex" href="/cart">
-				<button type="button" className="material-symbols-rounded" onClick={
-					() => {
-						dispatch({ type: 'TOGGLE_OPEN', isOpen: true })
-						router.push('/cart');
-					}
+	if (isClient && pathname !== '/cart') {
+
+		return (
+
+			<nav className="flex select-none items-center justify-center">
+				<NavLink classes="!mr-0 items-center justify-center flex" href="/cart">
+					<button type="button" className="material-symbols-rounded" onClick={
+						() => {
+							dispatch({ type: 'TOGGLE_OPEN', isOpen: true })
+							router.push('/cart');
+						}
 					}>Shopping_cart</button>
-				{state.items.length > 0 ? (
-					<span className="relative right-[0.2rem] -top-[1rem] flex items-center justify-center text-sm w-5 h-5 bg-emerald-700 rounded-full">{state.items.length}</span>
-				) : null }
-			</NavLink>
-		</nav>
+					{state.items.length > 0 ? (
+						<span className="relative right-[0.2rem] -top-[1rem] flex items-center justify-center text-sm w-5 h-5 bg-emerald-700 rounded-full">{state.items.length}</span>
+					) : null}
+				</NavLink>
+			</nav>
 
-	);
+		);
+	
+	} else if( isClient ) {
+
+		return (
+
+			<nav className="flex select-none items-center justify-center">
+				<NavLink classes="!mr-0 items-center justify-center flex" href="/">
+					<button type="button" className="material-symbols-rounded" onClick={
+						() => {
+							dispatch({ type: 'CHECKOUT', isOpen: true })
+							router.push('/');
+						}
+					}>keyboard_double_arrow_up</button>
+				</NavLink>
+		
+			</nav>
+			
+		);
+
+	}
 
 };
 
