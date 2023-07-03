96f5556a68433d641e6444a56632ec7e729aa834
jules-sommer <rcsrc@pm.me>
2023-07-03 00:25:33 -0400

    idek what to call these anymore

--- app/api/products/inStock/[bool]/route.js
+++ app/api/products/inStock/[bool]/route.js
@@ -3,80 +3,59 @@
 import { stdout } from "process";
 import clientPromise from "../../../../_utils/db"
 import { NextResponse } from "next/server";
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-const queries = async () => {
-
-    const products = await prisma.product.findMany({
-        where: {
-            inStock: {
-                value: {
-                    $gt: 0
-                }
-            }
-        }
-    })
-
-    console.log(products);
-    return products;
-
-}
+import { toNumber } from "lodash";
 
 export const GET = async (request, context) => {
 
-    let contextParams = context.params.bool;
+    let isInStock = context.params.bool === 'true' ? true : false;
 
-    const users = queries()
-        .then(async () => {
-            await prisma.$disconnect()
-        })
-        .catch(async (e) => {
-            console.error(e)
-            await prisma.$disconnect()
-            process.exit(1)
-        })
+    const client = await clientPromise;
+    const products = client.db('data').collection('products');
 
-    try {
 
-        const client = await clientPromise;
-        const products = client.db('data').collection('products');
+    let filter;
 
-    } catch(err) {
+    if( isInStock === true ) {
 
-        console.log(err);
+        filter = {
+            'inStock.value': {
+                '$gt': 0
+            }
+        };
+
+    } else if( isInStock === false ) {
+
+        filter = {
+            'inStock.value': {
+                '$eq': 0
+            }
+        };
 
     }
-
-    const filter = {
-        'inStock': {
-            '$elemMatch': {
-                '$eq': true
-            }
-        }
-    };
 
     const query = products.find(filter);
     
     try {
 
         const res = await query.toArray();
-        stdout.write(`/api/products/inStock/${contextParams} returned ${query}\n`);
+        stdout.write(`/api/products/inStock/${isInStock} returned ${JSON.stringify(res)}\n`);
     
         return NextResponse.json({
             success: true,
-            message: `/api/products/inStock/${contextParams} found ${res.length} products`,
+            message: `/api/products/inStock/${isInStock} found ${res.length} products`,
             data: res,
         }, { status: 200 });
 
     } catch(err) {
+
         console.log(err);
+
         return NextResponse.json({
             success: false,
-            message: `/api/products/inStock/${contextParams} errored`,
+            message: `/api/products/inStock/${isInStock} errored`,
             data: err,
         }, { status: 400 });
+
     }
 
 
